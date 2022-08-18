f8b967eefd582ba38088e52b67b3fedabbe61719
Stefano Locatelli <stloc22@student.sdu.dk>
2022-08-18 12:46:03 +0000

    performance 2

--- ci/performance-test.sh
+++ ci/performance-test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash            
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker_username=$DockerRepo docker-compose -f docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+docker_username=$DockerRepo docker-compose -f simple-fortune-cookie/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
