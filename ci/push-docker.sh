58ca3ead649e12b3f9db74de0d883d2805ba28b2
Luca Parimbelli <lupar22@student.sdu.dk>
2022-08-18 09:13:11 +0000

    trial1a

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
-docker push "$docker_username/simple-fortune-cookie:latest" &
+docker push "nadineml/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
+docker push "nadineml/simple-fortune-cookie:latest" &
 wait
