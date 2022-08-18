682cdcf6fb60f6dd82b53b4f0763fc70ccbb1f9c
Luca Parimbelli <lupar22@student.sdu.dk>
2022-08-18 10:23:56 +0000

    trial1h

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker images --all
-docker push "nadineml/simple-fortune-cookie_frontend:latest"
-docker push "nadineml/simple-fortune-cookie_backend:latest"
+docker simple-fortune-cookie_backend push "nadineml/fortune-cookie-backend:latest"
+docker simple-fortune-cookie_frontend push "nadineml/fortune-cookie-frontend:latest"
