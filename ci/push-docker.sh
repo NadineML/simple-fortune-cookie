092bb8d491f0fc116f1607284a4a537a00f668b4
Luca Parimbelli <lupar22@student.sdu.dk>
2022-08-18 10:48:53 +0000

    test dockerhub images 4

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/devops-academy-handins-public/simple-fortune-cookie/frontend
+cd frontend
 docker build -t nadineml/fortune-cookie-frontend .
 docker push nadineml/fortune-cookie-frontend
-
-cd /home/ubuntu/devops-academy-handins-public/simple-fortune-cookie/backend
-docker build -t nadineml/fortune-cookie-backend .
-docker push nadineml/fortune-cookie-backend
